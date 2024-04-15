7c40ad6c55c3ea0e739fd5f0266eda36cdf7966d
Emmanuel Yee <emmanuel_yee_hong_wei@s2022.ssts.edu.sg>
2024-04-15 14:54:42 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,6 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
       <a href="https://github.com/Liquefy7822/warhammer-dice-roller/blob/main/components/Footer.js" target="_blank" rel="noopener noreferrer">View on GitHub</a>
     </footer>
   )
