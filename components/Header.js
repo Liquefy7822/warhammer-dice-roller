9da91571c4a55a245aa5dab4a6cf7528ee98a166
Emmanuel Yee <emmanuel_yee_hong_wei@s2022.ssts.edu.sg>
2024-04-15 16:55:44 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,18 @@
+// components/Header.js
+
+import Link from 'next/link';
+
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return (
+    <div>
+      <h1 className="title">{title}</h1>
+      <div className="phase-buttons">
+        <Link href="/command-phase"><a><button className="phase-button">Command Phase</button></a></Link>
+        <Link href="/movement-phase"><a><button className="phase-button">Movement Phase</button></a></Link>
+        <Link href="/shooting-phase"><a><button className="phase-button">Shooting Phase</button></a></Link>
+        <Link href="/charge-phase"><a><button className="phase-button">Charge Phase</button></a></Link>
+        <Link href="/fight-phase"><a><button className="phase-button">Fight Phase</button></a></Link>
+      </div>
+    </div>
+  );
 }
