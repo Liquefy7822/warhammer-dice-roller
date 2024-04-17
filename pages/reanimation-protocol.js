255214f55705f65fbb6920d22cab51913db98eef
Emmanuel Yee <emmanuel_yee_hong_wei@s2022.ssts.edu.sg>
2024-04-17 19:48:05 +0800

    Update reanimation-protocol.js

--- pages/reanimation-protocol.js
+++ pages/reanimation-protocol.js
@@ -1,11 +1,10 @@
-// pages/reanimation-protocols.js
-
 import React, { useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const ReanimationProtocols = () => {
   const [unitCount, setUnitCount] = useState(1);
+  const [allMaxHealth, setAllMaxHealth] = useState(false);
   const [reanimationResult, setReanimationResult] = useState('');
 
   const rollDice = () => {
@@ -17,11 +16,19 @@ const ReanimationProtocols = () => {
     let totalModelsAdded = 0;
     for (let i = 0; i < unitCount; i++) {
       const roll = rollDice();
-      totalWounds += roll;
+      // Dividing the result by 2 and rounding up to get the D3 result
+      const wounds = Math.ceil(roll / 2);
+      totalWounds += wounds;
       if (roll === 1 && totalModelsAdded < unitCount) {
         totalModelsAdded++;
       }
     }
+
+    if (allMaxHealth) {
+      totalWounds += totalModelsAdded;
+      totalModelsAdded = 0;
+    }
+
     setReanimationResult(`Total wounds reanimated: ${totalWounds}. Total models added: ${totalModelsAdded}`);
   };
 
@@ -35,6 +42,15 @@ const ReanimationProtocols = () => {
           <input type="number" value={unitCount} onChange={(e) => setUnitCount(parseInt(e.target.value))} />
         </label>
       </div>
+      <div>
+        <input
+          type="checkbox"
+          id="allMaxHealth"
+          checked={allMaxHealth}
+          onChange={(e) => setAllMaxHealth(e.target.checked)}
+        />
+        <label htmlFor="allMaxHealth">All models are at max health</label>
+      </div>
       <button onClick={performReanimation}>Perform Reanimation</button>
       {reanimationResult && (
         <p>{reanimationResult}</p>
@@ -45,3 +61,4 @@ const ReanimationProtocols = () => {
 };
 
 export default ReanimationProtocols;
+
