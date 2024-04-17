b79dbec85d3386076d9e5c02ee7e877453c4ad38
Emmanuel Yee <emmanuel_yee_hong_wei@s2022.ssts.edu.sg>
2024-04-17 19:53:19 +0800

    Update reanimation-protocol.js

--- pages/reanimation-protocol.js
+++ pages/reanimation-protocol.js
@@ -3,7 +3,6 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const ReanimationProtocols = () => {
-  const [unitCount, setUnitCount] = useState(1);
   const [allMaxHealth, setAllMaxHealth] = useState(false);
   const [reanimationResult, setReanimationResult] = useState('');
 
@@ -12,36 +11,21 @@ const ReanimationProtocols = () => {
   };
 
   const performReanimation = () => {
-    let totalWounds = 0;
-    let totalModelsAdded = 0;
-    for (let i = 0; i < unitCount; i++) {
-      const roll = rollDice();
-      // Dividing the result by 2 and rounding up to get the D3 result
-      const wounds = Math.ceil(roll / 2);
-      totalWounds += wounds;
-      if (roll === 1 && totalModelsAdded < unitCount) {
-        totalModelsAdded++;
-      }
-    }
+    const roll = rollDice();
+    // Dividing the result by 2 and rounding up to get the D3 result
+    const wounds = Math.ceil(roll / 2);
 
     if (allMaxHealth) {
-      totalWounds += totalModelsAdded;
-      totalModelsAdded = 0;
+      setReanimationResult(`Revive all units in the group`);
+    } else {
+      setReanimationResult(`Total wounds reanimated: ${wounds}`);
     }
-
-    setReanimationResult(`Total wounds reanimated: ${totalWounds}. Total models added: ${totalModelsAdded}`);
   };
 
   return (
     <div style={{ textAlign: 'center' }}>
       <Header title="Reanimation Protocols" />
       <h2>Unit Reanimation</h2>
-      <div>
-        <label>
-          Number of Units:
-          <input type="number" value={unitCount} onChange={(e) => setUnitCount(parseInt(e.target.value))} />
-        </label>
-      </div>
       <div>
         <input
           type="checkbox"
